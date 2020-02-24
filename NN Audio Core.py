4b3522f09c3b5430bc3a4169501834a36d126021
DaveWeih <dave_weih@hotmail.com>
2020-02-23 20:47:17 -0800

    Major changes to switch over to pitch bins for core, data generation, and training. Network builds and ran for part of an epoch - switching over to colab for real training Still have to do round trip evaluation

--- NN Audio Core.py
+++ NN Audio Core.py
@@ -11,8 +11,8 @@
 import librosa
 import numpy as np
 import cmath
+import math
 import os
-import keras.backend as K
 
 # Constants and settings
 DTYPE = 'float32'
@@ -21,34 +21,15 @@ WINDOW_SIZE = 55  # Has to be odd
 TARGET_COL = WINDOW_SIZE//2
 
 # stft values
-#N_FFT = 1024 # 512 recommended for speech, music typically 2048
-#FFT_BINS = 513
+N_FFT = 1024 # 512 recommended for speech, music typically 2048
+FFT_BINS = 513
 
-# cqt values
 HOP_LENGTH = 128 # Required for good round trip quality
-
-BINS_PER_OCTAVE = 12 * 5
-FMIN = librosa.note_to_hz('C1') # Could probably cut further up, but doesn't work in librosa
-OCTAVES = 8
-
-FFT_BINS = OCTAVES * BINS_PER_OCTAVE # function of items below
-
 
 # Shared functions
 
-# Idea here is that we operate on magnitude, and will just use phase from the original noisy sample
-# NOTE that this only seems to work well with stft, not cqt
-def rebuild_fft(output, original_fft):
-    vphase = np.vectorize(cmath.phase)
-    o_phase = vphase(original_fft)
-    mag = output.T
-    vrect = np.vectorize(cmath.rect)
-    return vrect(mag, o_phase)
-
-
 def get_ft(wav):
-    c = librosa.cqt(wav, hop_length=HOP_LENGTH, fmin=FMIN, n_bins=OCTAVES*BINS_PER_OCTAVE, bins_per_octave=BINS_PER_OCTAVE)
-    #c = librosa.stft(wav, hop_length=HOP_LENGTH, n_fft=N_FFT)
+    c = librosa.stft(wav, hop_length=HOP_LENGTH, n_fft=N_FFT)
     return c
 
 def get_ft_from_file(file):
@@ -57,66 +38,98 @@ def get_ft_from_file(file):
     return get_ft(wav)
 
 def inv_ft(ft):
-    return librosa.icqt(ft, hop_length=HOP_LENGTH, fmin=FMIN, bins_per_octave=BINS_PER_OCTAVE)
-    #return librosa.istft(ft, hop_length=HOP_LENGTH)
+    return librosa.istft(ft, hop_length=HOP_LENGTH)
 
 # This is an approximation - much better ways to compare voice quality exist, but this works fine
 def diff_ft(ft1, ft2):
     per_sample = np.sum(abs(ft1-ft2), axis=0)
     return np.average(per_sample)
 
-# Returns magnitude and phase as x & y on unit circle (for continuous error measurement)
+
+################################################################################
+# Pitch & sample encoding functions
+
+# Low cut off at bin 3 ~65hz (C2) - seems like 125hz should be safe but keeping the low end
+# High cut off bin 300 ~6500hz - 5000hz sounded OK, but keeping some just in case
+# These are based on N_FFT 1024
+LOW_BIN = 3 
+HIGH_BIN = 340
+SAMPLE_BINS = HIGH_BIN-LOW_BIN
+
+freq = librosa.fft_frequencies(sr=22050, n_fft=N_FFT)
+LOW_FREQ = freq[LOW_BIN]
+HIGH_FREQ = freq[HIGH_BIN]
+SAMPLE_OCTAVES = math.log(HIGH_FREQ/LOW_FREQ,2)
+BINS_PER_OCTAVE = 20  # Tested empirically this sounds fine (15 was also OK so this should be safe)
+
+
+# Number of bins in rescaled pitch representation of the ft
+PITCH_BINS = math.floor(SAMPLE_OCTAVES * BINS_PER_OCTAVE)
+
+# S = sample space, which is frequency  P = pitch space, which is rescaled so pitches are constant space appart
+def S_ix(p_ix):
+    return SAMPLE_OCTAVES * ((SAMPLE_BINS/SAMPLE_OCTAVES + 1)**(p_ix/PITCH_BINS) -1 )
+
+def P_ix(s_ix):
+    return PITCH_BINS * math.log((1+s_ix/SAMPLE_OCTAVES),2)/math.log((1 + SAMPLE_BINS/SAMPLE_OCTAVES),2)
+
+# Project X onto returned array Y using a list of ascending ys of len(X) with maximum value of leng(Y), so that
+# where multiple ys's map to a single index i in X, assign X[i] to Y[those ys's]
+# where a single yx maps to multiple indices in X, assign X[these i's] of X[ys]
+def squash_stretch(ys, X, Ylen):
+    Y = np.zeros((Ylen))
+    i = 0
+    while i < (len(ys)-1):
+        xh = i
+        if (math.floor(ys[i+1]) > math.floor(ys[i]+1)):
+            #print("Batch assigning X[i] to Y[ys[i]:ys[i+1]] ",i, ys[i], ys[i+1])
+            Y[ys[i]:ys[i+1]] = X[i]
+            i += 1
+        else:
+            while (math.floor(ys[i]) == math.floor(ys[xh])) and (xh < len(ys)-1):
+                xh += 1
+            if (xh > i+1):
+                Y[ys[i]] = 0
+                #print("Averaging X[i,xh] into Y[ys[i]] ", i, xh, ys[i])
+                Y[ys[i]] = np.average(X[i:xh])
+                i = xh
+            else:
+                #print("Straight map X[i] to Y[ys[i]] ", i, ys[i])
+                Y[ys[i]] = X[i]
+                i += 1
+        #print (i, ys[i], ys[xh])
+    return Y
+
+# Takes samples and rescales to pitch (log of frequencies so space between same pitch is constant)
+# Output is samples, then rescaled magnitudes in shape (sample_count, PITCH_BINS)
+def pitch_scale(st):
+    si = list(range(SAMPLE_BINS))
+    pi = [math.floor(P_ix(x)) for x in si]
+    pt = np.empty((st.shape[0], PITCH_BINS))
+    for i in range(st.shape[0]):
+        pt[i,:] = squash_stretch(pi, st[i,:], PITCH_BINS)
+    return pt
+
+def sample_scale(pt):
+    pi = list(range(PITCH_BINS))
+    si = [math.floor(S_ix(x)) for x in pi]
+    st = np.empty((pt.shape[0],SAMPLE_BINS))
+    for i in range(pt.shape[0]):
+        st[i,:] = squash_stretch(si, pt[i,:], SAMPLE_BINS)
+    return st
+
+# Sample output is ft converted to magnitude and .T to get samples then ft
+# Only returns 'interesting' samples - between LOW_ and HIGH_ bins, so output shape is (sample_count, SAMPLE_BINS)
 def get_samples(file):
     wav, rate = librosa.core.load(file)
-    ft = get_ft(wav)
-    polar_vect = np.vectorize(cmath.polar)
-    M, P = polar_vect(ft)
-    # organized as bins, frames so we need to transpose first two axes to frames, bins
-    samples = np.empty((M.shape[1],M.shape[0],3))
-    samples[:,:,0] = M.T 
-    samples[:,:,1] = np.cos(P).T
-    samples[:,:,2] = np.sin(P).T
-    return samples 
+    samples = abs(get_ft(wav).T) # organized as bins, frames so we need to transpose them to frames, bins
+    return samples[:,LOW_BIN:HIGH_BIN]
 
-def rebuild_cqt(output):
-    M = output[:,:,0]
-    R = M * output[:,:,1]
-    I = M * output[:,:,2] * 1j 
-    cqt = R + I
-    return cqt.T
-    
-
-
-
-
-# Cut all this if I don't use it
-
-def magnitude(X, axis):
-    X2 = K.square(X)
-    M2 = K.sum(X2, axis=axis, keepdims=True)
-    return K.sqrt(M2)
-
-# Punish this for being away from target magnitude
-# Magic number of 0.5 for relative importance of components vs. magnitude
-def magnitude_loss(y_actual, y_predicted):
-    ya_magnitude = magnitude(y_actual,2)
-    yp_magnitude = magnitude(y_predicted,2)
-    mag_loss = K.mean(K.square(ya_magnitude-yp_magnitude))
-    component_loss = K.mean(K.square(y_actual-y_predicted))
-    return mag_loss + 0.5 * component_loss
-
-# Solution for problems reimporting with custom loss function from https://github.com/keras-team/keras/issues/5916
-import keras.losses
-keras.losses.magnitude_loss = magnitude_loss
-
-# Cruft to evaluate deleting
-#def filter(cqt):
-#    cqt[0:BINS_PER_OCTAVE,:] = 0
-#    return cqt
-
-# build up as (bins, samples) then transpose to model view of (samples, bins)
-#def targets_to_fft(targets):
-#    fft = np.empty((targets.shape[0],targets.shape[1]//2), dtype='complex64')
-#    for i in range(0, targets.shape[0]):
-#        fft[i] = combine_target(targets[i])
-#    return fft.T   # transpose
+def rebuild_fft(samples, original_fft):
+    fft = np.zeros((samples.shape[0], FFT_BINS))
+    fft[:,LOW_BIN:HIGH_BIN] = samples
+    vphase = np.vectorize(cmath.phase)
+    o_phase = vphase(original_fft)
+    mag = fft.T
+    vrect = np.vectorize(cmath.rect)
+    return vrect(mag, o_phase)
